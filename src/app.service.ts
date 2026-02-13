142b886124552331f8a36bb1bf7f688567f7fff0
alonso536 <josealonsodiazleiva@gmail.com>
2026-02-13 15:24:08 -0300

    rename

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! 12131321312';
+    return 'Hello World! from Google Cloud Platform';
   }
 }
